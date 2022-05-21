ebd9f7996e6a7a51b560ee5babddc3d33bfda902
LittleOuane <Littlemanstudioofficial@gmail.com>
2022-05-22 00:48:39 +0200

    implemented basic anagram solution research

--- README.md
+++ README.md
@@ -6,7 +6,3 @@ This program is meant to find, among a certain list of words given in words.txt,
 I'm making this program to train myself on files reading using python.
 
 By giving uppercases letters in the word you give, the program will guess your word more efficiently by considering that this is the first letter.
-
-In words.txt, you need to display the words this way :
-
-apple,banana,chair,dodo,enigmatic,far,geometry,hyperspace,immersive,john,kiwi,limbo,method,nanometer,optimusprime,prison,quote,rabbit,star,tetraedron,unicorn,vampire,wombat,xenolith,yoyo,zebra
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import sys
-
-raw_words = open("words.txt").readlines()
-
-print(raw_words)
+from itertools import permutations
 
 def main() :
-    pass
+    raw_words = open("words.txt").readlines()
+
+    words = set()
+    for word in raw_words :
+        words.add(word[0:-1])
+
+    anagram = input("Enter your anagram ")
+
+    anagram_permutations = permutations(anagram)
+
+    for permutation in anagram_permutations :
+        formated_permutation = ""
+        for letter in permutation :
+            formated_permutation += letter
+
+        if formated_permutation in words :
+            solution = formated_permutation
+        
+    print(f"the solution is {solution}")
 
 if __name__ == "__main__" :
     main()
